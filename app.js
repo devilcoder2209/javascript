8d687b0c81211c0e3ab6f287f0e665addcb1bfcd
devilcoder2209 <81303656+devilcoder2209@users.noreply.github.com>
2021-09-02 19:35:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log({ name: "Saumyadeep" })
 console.log(true)
 console.log(false)
 
-console.log(false, 234243, 'string', { name: "Alfred" })
+console.log(false, 234243, 'string', { name: "Chakrabarty" })
 
 console.warn("This is a warning")
 
